c544394edfc4436302c98290cd3cb8aa063aa949
Abdelmageed Ahmed <117668043+AbdelmageedIzzat@users.noreply.github.com>
2025-12-31 23:49:03 -0800

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+// js/firebase-config.js
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY_HERE",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
